97e31eff4a1ec1af61c5b59fafb54cfee084c0d7
nbtm-sh <fuzzy.hazza.gld@protonmail.com>
2021-06-01 03:55:24 +1000

    ...

--- js/index.js
+++ js/index.js
@@ -1,5 +1,24 @@
 var express = require("express");
+var sql_db = require("./mysql_api.js")
 
 var app = express();
+
+function send_matches(results, client_res) {
+    var res_json = [];
+    console.log(results);
+}
+
+app.get('/matches', (req, res) => {
+    var player1 = req.query.u1;
+    var player2 = req.query.u2;
+
+    var query = `SELECT * FROM \`matches\` WHERE (uPlayer1=${player1} OR uPlayer2=${player1}) AND (uPlayer1=${player2} OR uPlayer2=${player2});`;
+    sql_db.query(query, send_matches, res);
+});
+
+app.get('/all', (req, res) => {
+    var query = 'SELECT * FROM `matches`;';
+    sql_db.query(query, send_matches, res);
+})
 
 var server = app.listen(8080);
---
+++ js/mysql_api.js
@@ -0,0 +1,19 @@
+var creds = require("./mysql-creds.json");
+var mysql = require("mysql");
+
+var mysql_connection = mysql.createConnection({
+    "host": "localhost",
+    "user": creds.username,
+    "password": creds.password,
+    "database": "stf"
+});
+
+mysql_connection.connect();
+
+function query(sql_query, callback, ext_args) {
+    mysql.query(sql_query, (err, result, fields, cb=callback, ext=ext_args) => {
+        if (!err) {
+            cb(result, ext_args);
+        }
+    })
+}
