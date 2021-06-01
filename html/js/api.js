7b08d93613d3b86bfae553e3df892300cd0230f9
nbtm-sh <fuzzy.hazza.gld@protonmail.com>
2021-06-01 23:22:37 +1000

    ...

--- html/js/api.js
+++ html/js/api.js
@@ -61,14 +61,14 @@ class API {
 
         for (var i = 0; i < this.matches.length; i += 1) {
             htmlData += `
-                <a href="/match?m=${this.matches[i].id.toString()}"><tr>
+                <tr><a href="/match?m=${this.matches[i].id.toString()}">
                     <td data="player1" >${this.matches[i].uPlayer1.uName}</td>
                     <td data="player1Figher">${this.matches[i].uPlayer1Fighter}</td>
                     <td data="winner">${this.matches[i].tResult.uName}</td>
                     <td data="winner">${this.matches[i].tRound}</td>
                     <td data="player1Figher">${this.matches[i].uPlayer2Fighter}</td>
                     <td data="player1" >${this.matches[i].uPlayer2.uName}</td>
-                </tr></a>
+                </a></tr>
             `;
         }
 
