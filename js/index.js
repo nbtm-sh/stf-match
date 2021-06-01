7d03a99324a58ffe7bc2de5641595a6070d2b99a
nbtm-sh <fuzzy.hazza.gld@protonmail.com>
2021-06-01 20:02:26 +1000

    ...

--- js/index.js
+++ js/index.js
@@ -79,6 +79,8 @@ app.get('/player', (req, res, query_callback=null, msql=mysql_connection) => {
     global_temp = [];
     for (var i = 0; i < ids.length; i += 1) {
         console.log(ids[i]);
+        console.log(i);
+        console.log((i>=ids.length-1));
         msql.query(`SELECT * FROM \`players\` WHERE id=${ids[i]};`, (err, result, fields, cb=send_players, ext=res, send_results=(i>=ids.length-1), gt=global_temp, len=i) => {
             gt.push(result);
             if (send_results) {
