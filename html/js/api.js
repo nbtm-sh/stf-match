1796faaef7e8b71fe07e60306e200b10addf44dc
nbtm-sh <fuzzy.hazza.gld@protonmail.com>
2021-06-01 22:17:26 +1000

    ...

--- html/js/api.js
+++ html/js/api.js
@@ -5,7 +5,11 @@ class API {
 
     }
 
-    getAllUserIds() {
-        
+    getAllUserData(this) {
+        WebRequest.get("http://stf.nbti.net:8080/players", this.cb_getAllUserData, this);
+    }
+
+    cb_getAllUserData(this, st) {
+        this.users = JSON.parse(st);
     }
 }
