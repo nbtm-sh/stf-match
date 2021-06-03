dbf1511af35cd5940043ef9ccd6f4d93dc7002be
nbtm-sh <fuzzy.hazza.gld@protonmail.com>
2021-06-04 00:52:58 +1000

    ...

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -29,17 +29,17 @@ CREATE TABLE `matches` (
     CONSTRAINT `fk_tournament_id`
         FOREIGN KEY (tTournament) REFERENCES tournaments (id)
         ON UPDATE NO ACTION
-        ON DELETE CASCADE,
+        ON DELETE NO ACTION,
     
     CONSTRAINT `fk_player1_id`
         FOREIGN KEY (uPlayer1) REFERENCES players (id)
         ON UPDATE NO ACTION
-        ON DELETE CASCADE,
+        ON DELETE NO ACTION,
     
     CONSTRAINT `fk_player2_id`
         FOREIGN KEY (uPlayer2) REFERENCES players (id)
         ON UPDATE NO ACTION
-        ON DELETE CASCADE
+        ON DELETE NO ACTION
 );
 
 CREATE TABLE `individualMatches` ( -- Actual match results
@@ -54,10 +54,10 @@ CREATE TABLE `individualMatches` ( -- Actual match results
     CONSTRAINT `fk_match_id`
         FOREIGN KEY (tMatch) REFERENCES matches (id)
         ON UPDATE NO ACTION
-        ON DELETE CASCADE,
+        ON DELETE NO ACTION,
     
     CONSTRAINT `fk_tournament_id`
         FOREIGN KEY (tTournament) REFERENCES tournaments (id)
         ON UPDATE NO ACTION
-        ON DELETE CASCADE
+        ON DELETE NO ACTION
 );
