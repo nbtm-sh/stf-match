abaad5d2d6b7b8d46bfd00843f432cf5c3bb3fb6
nbtm-sh <fuzzy.hazza.gld@protonmail.com>
2021-06-05 02:18:46 +1000

    ...

--- rest/player.py
+++ rest/player.py
@@ -1,8 +1,9 @@
 class Player:
-    def __init__(self, id=None, uName=None, uCountry=None):
+    def __init__(self, id=None, uName=None, uCountry=None, uTeam=None):
         self.id = id
         self.uName = uName
         self.uCountry = uCountry
+        self.uTeam = None
         self.qualified = True
     
     def json(self):
@@ -10,5 +11,6 @@ class Player:
             "id": self.id,
             "uName": self.uName,
             "uCountry": self.uCountry,
+            "uTeam": self.uTeam,
             "bQualified": self.qualified
         }
