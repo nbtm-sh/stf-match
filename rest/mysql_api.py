db39d45888ad711af4b5487ff6bfd67fa4f8d307
nbtm-sh <fuzzy.hazza.gld@protonmail.com>
2021-06-07 02:47:41 +1000

    ...

--- rest/mysql_api.py
+++ rest/mysql_api.py
@@ -73,7 +73,7 @@ class SQLAPI:
             result_object.tName = i[1]
             result_object.tLocation = i[2]
             result_object.uParticipants = self.get_tournament_participants(result_object)
-            result_object.tMatches = self.get_matches_by_tournament(result_object) if True else None
+            result_object.tMatches = self.get_matches_by_tournament(result_object) if not exclude_matches else None
         
         return result_object
     
